7b91443fa2cee0393f1fa4ec315096618a7279b6
richen604 <56615615+richen604@users.noreply.github.com>
2021-03-19 09:28:01 -0700

    init nodemon for development

--- index.ts
+++ index.ts
@@ -1,18 +1,20 @@
-import express from 'express';
-import diagnoseRouter from './routes/diagnoses';
-import patientRouter from './routes/patients';
+import express from "express";
+import cors from "cors";
+import diagnoseRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
 });
 
-app.use('/api/diagnoses', diagnoseRouter);
-app.use('/api/patients', patientRouter);
+app.use("/api/diagnoses", diagnoseRouter);
+app.use("/api/patients", patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
