743d0a695de01d738453dd8dea8c5b21ec68ecf2
richen604 <56615615+richen604@users.noreply.github.com>
2021-03-20 13:36:18 -0700

    fixed produdction uri, added param to tsconfigReset

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,8 @@
-export const apiBaseUrl = 'http://localhost:3000/api';
+export let apiBaseUrl: string;
+
+if (process.env.NODE_ENV === "development") {
+  apiBaseUrl = "http://localhost:3000/api";
+}
+if (process.env.NODE_ENV === "production") {
+  apiBaseUrl = "/api";
+}
