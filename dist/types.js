811cc0fc976a74e3083b53d8f660d5db07dd874c
richen604 <56615615+richen604@users.noreply.github.com>
2021-03-20 13:00:14 -0700

    reestablished git for /gitignore

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["male"] = "male";
-    Gender["female"] = "female";
-    Gender["other"] = "other";
-})(Gender = exports.Gender || (exports.Gender = {}));
